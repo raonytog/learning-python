76b64c0c1a7ab080b23306d45d3324de971b4667
raonytog <raony.edu@edu.ufes.br>
2024-01-19 22:13:50 -0300

    toca musica hehe

--- Aulas basicas/Desafios/Desafio21.py
+++ Aulas basicas/Desafios/Desafio21.py
@@ -1,5 +1,26 @@
-# busca e toca um audio em mp3 
+# busca e toca um audio em mp3
 
-import vlc
+from pygame import mixer
 
+mixer.init()
+mixer.music.load('glueSong.mp3')
+print("starting music...")
+mixer.music.set_volume(0.2)
+mixer.music.play()
 
+while (True):
+    print("-"*50)
+    print("Type 'p' to pause")
+    print("Type 'r' to resume")
+    print("Type anything else end the program")
+    print("-"*50)
+    
+    choice = input("Type what you want: ")
+    if (choice == 'p'): 
+        mixer.music.pause()
+        
+    elif (choice == 'r'):
+        mixer.music.unpause()
+        
+    else:
+        break
