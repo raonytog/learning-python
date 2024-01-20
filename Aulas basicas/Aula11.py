16116999fb818c9359a76ad96dbc7669bee8831a
raonytog <raony.edu@edu.ufes.br>
2024-01-20 17:54:27 -0300

    Desafios da aula 10 (condicionais)

---
+++ Aulas basicas/Aula11.py
@@ -0,0 +1,20 @@
+# cores no terminal
+
+# style:text:back
+
+# styles: 0 (nada), 1 (negrito), 4 (sublinhado), 7 (inverte as configuracoes)
+# text                    background
+# 30      black           preto          40
+# 31      red             vermelho       41
+# 32      green           verde          42
+# 33      yellow          amarelo        43
+# 34      blue            azul           44
+# 35      Magenta         magenta        45
+# 36      cyan            ciano          46
+# 37      grey            cinza          47
+# 97      white           branco         107
+
+
+print(f'Ola, \033[1;36;107mRaony\033[m!!')
+
+
